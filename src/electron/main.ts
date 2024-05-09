0a36050f3445a91ffb7dbbc59d119b8916a9575e
Niklas Ziermann <n-ziermann@protonmail.com>
2024-05-09 12:02:36 +0200

    use typescript for electron code

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 
+type test = string;
+
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
   mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
