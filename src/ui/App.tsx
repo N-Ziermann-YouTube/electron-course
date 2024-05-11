b7b54480c473622088a3323e58eed506d7adcae9
Niklas Ziermann <n-ziermann@protonmail.com>
2024-05-11 18:38:09 +0200

    start dev process using singular dev script

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <div>
-        1
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
