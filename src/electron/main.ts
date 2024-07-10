7f10d5e3138c2c655e4101513ed7b3d1c5dc4283
Niklas Ziermann <n-ziermann@protonmail.com>
2024-07-10 21:58:03 +0200

    implement close-events to enable hide to tray functionality

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -28,4 +28,29 @@ app.on('ready', () => {
       process.platform === 'darwin' ? 'trayIconTemplate.png' : 'trayIcon.png'
     )
   );
+
+  handleCloseEvents(mainWindow);
 });
+
+function handleCloseEvents(mainWindow: BrowserWindow) {
+  let willClose = false;
+
+  mainWindow.on('close', (e) => {
+    if (willClose) {
+      return;
+    }
+    e.preventDefault();
+    mainWindow.hide();
+    if (app.dock) {
+      app.dock.hide();
+    }
+  });
+
+  app.on('before-quit', () => {
+    willClose = true;
+  });
+
+  mainWindow.on('show', () => {
+    willClose = false;
+  });
+}
