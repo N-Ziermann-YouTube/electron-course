56c234e73762cff29f70be2adca0f6defcd253c2
Niklas Ziermann <n-ziermann@protonmail.com>
2024-08-19 21:52:30 +0200

    implement playwright tests

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,20 +1,50 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, _electron } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+let electronApp: Awaited<ReturnType<typeof _electron.launch>>;
+let mainPage: Awaited<ReturnType<typeof electronApp.firstWindow>>;
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+async function waitForPreloadScript() {
+  return new Promise((resolve) => {
+    const interval = setInterval(async () => {
+      const electronBridge = await mainPage.evaluate(() => {
+        return (window as Window & { electron?: any }).electron;
+      });
+      if (electronBridge) {
+        clearInterval(interval);
+        resolve(true);
+      }
+    }, 100);
+  });
+}
+
+test.beforeEach(async () => {
+  electronApp = await _electron.launch({
+    args: ['.'],
+    env: { NODE_ENV: 'development' },
+  });
+  mainPage = await electronApp.firstWindow();
+  await waitForPreloadScript();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.afterEach(async () => {
+  await electronApp.close();
+});
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+test('custom frame should minimize the mainWindow', async () => {
+  await mainPage.click('#minimize');
+  const isMinimized = await electronApp.evaluate((electron) => {
+    return electron.BrowserWindow.getAllWindows()[0].isMinimized();
+  });
+  expect(isMinimized).toBeTruthy();
+});
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole('heading', { name: 'Installation' })
-  ).toBeVisible();
+test('should create a custom menu', async () => {
+  const menu = await electronApp.evaluate((electron) => {
+    return electron.Menu.getApplicationMenu();
+  });
+  expect(menu).not.toBeNull();
+  expect(menu?.items).toHaveLength(2);
+  expect(menu?.items[0].submenu?.items).toHaveLength(2);
+  expect(menu?.items[1].submenu?.items).toHaveLength(3);
+  expect(menu?.items[1].label).toBe('View');
 });
